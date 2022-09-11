99ffb43d297f5317284f2d44b54fd987779a80ab
rintha <rintharithu@gmail.com>
2022-09-11 12:32:37 +0530

    delete edit

--- project1/dom.js
+++ project1/dom.js
@@ -1,115 +1,40 @@
-// // console.dir(document);
-// console.log(document.title);
-// document.title='Items';
-// // console.log(document.forms);
-// // console.log(document.getElementById('header-title'));
-// var headerTitle=document.getElementById('header-title');
-// var header=document.getElementById('main-header')
-// // console.log(headerTitle);
-// header.style.borderBottom='solid 3px #000'
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
-// var items=document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent='hello 2';
-// items[1].style.fontweight='bold';
-// items[1].style.backgroundColor='yellow'
-// items[3].style.backgroundColor='green';
-// items.style.fontweight='bold'
-// for(var i=0;i<items.length;i++){
-//     items[i].style.fontweight='bold'
-// }
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
+// Add item
+function addItem(e){
+  e.preventDefault();
 
-// var li=document.getElementsByTagName('li');
-// console.log(li);
-// console.log(li[1]);
-// li[1].textContent='hello 2';
-// li[1].style.fontweight='bold';
-// li[1].style.backgroundColor='yellow'
-// li[3].style.backgroundColor='green';
+  // Get input value
+  var newItem = document.getElementById('item').value;
 
-// for(var i=0;i<li.length;i++){
-//     li[i].style.fontweight='bold'
-// }
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
 
-// var header= document.querySelector("#main-header");
-// header.style.borderBottom='solid 3px #ccc';
-// var item = document.querySelector(".list-group-item");
-// item.style.color='red';
+  // Create del button element
+  var deleteBtn = document.createElement('button');
 
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
-// var odd=document.querySelectorAll('li:nth-child(odd)');
-// for(var i=0; i<odd.length; i++){
-//     odd[i].style.backgroundColor ='green';
-   
-// }
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
 
-// // TRAVERSING THE DOM //
-// var itemList = document.querySelector('#items');
-// parentNode
-// console.log(itemList.parentNode);
-// itemList.parentNode.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentNode.parentNode.parentNode);
+  // Append button to li
+  li.appendChild(deleteBtn);
 
-// parentElement
-// console.log(itemList.parentElement);
-// itemList.parentElement.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentElement.parentElement.parentElement);
-
-// childNodes
-// console.log(itemList.childNodes);
-
-// console.log(itemList.children);
-// console.log(itemList.children[1]);
-// itemList.children[1].style.backgroundColor = 'yellow';
-
-// // FirstChild
-// console.log(itemList.firstChild);
-// // firstElementChild
-// console.log(itemList.firstElementChild);
-// itemList.firstElementChild.textContent = 'Hello 1';
-
-
-// lastChild
-// console.log(itemList.lastChild);
-// lastElementChild
-// console.log(itemList.lastElementChild);
-// itemList.lastElementChild.textContent = 'Hello 4';
-
-// nextSibling
-// console.log(itemList.nextSibling);
-// // nextElementSibling
-// console.log(itemList.nextElementSibling);
-
-// previousSibling
-// console.log(itemList.previousSibling);
-// previousElementSibling
-// console.log(itemList.previousElementSibling);itemList.previousElementSibling.style.color = 'green';
-
-// Create a div
-// var newDiv =  document.createElement('div');
-
-// // Add class
-// newDiv.className= 'hello';
-
-// // Add id
-// newDiv.id = 'hello1';
-
-// // Add attr
-// newDiv.setAttribute('title', 'Hello Div');
-
-// // Create text node
-// var newDivText = document.createTextNode('Hello World');
-
-// // Add text to div
-// newDiv.appendChild(newDivText);
-
-// var container = document.querySelector('header .container');
-// var h1 = document.querySelector('header h1');
-
-// console.log(newDiv);
-
-// newDiv.style.fontSize = '30px';
-
-// container.insertBefore(newDiv, h1);
+  // Append li to list
+  itemList.appendChild(li);
+}
